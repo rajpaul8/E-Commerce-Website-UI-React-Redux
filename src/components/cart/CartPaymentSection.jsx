b9f3d6f68f94fe4c3748bab07c3b24cdefa1ffdf
rajpaul8 <rajpaul83@gmail.com>
2022-08-03 17:16:40 +0530

    toFix fixed in payment cart

--- src/components/cart/CartPaymentSection.jsx
+++ src/components/cart/CartPaymentSection.jsx
@@ -60,7 +60,7 @@ function CartPaymentSection() {
                             </tr>
                             <tr >
                                 <td className='text-base font-light'>Estimated Tax</td>
-                                <td className='flex justify-end'>${cartTotalPriceAmountWithoutDiscount&&((cartTotalPriceAmountWithoutDiscount * 12) / 100)}</td>
+                                <td className='flex justify-end'>${cartTotalPriceAmountWithoutDiscount && ((cartTotalPriceAmountWithoutDiscount * 12) / 100).toFixed(2)}</td>
                             </tr>
                             <tr >
                                 <td className='text-base font-light'>Estimated Shipping</td>
@@ -68,7 +68,7 @@ function CartPaymentSection() {
                             </tr>
                             <tr >
                                 <td className='text-base font-bold'>Estimated Total</td>
-                                <td className='flex justify-end font-bold'>$ {cartTotalPriceAmountWithoutDiscount&&((cartTotalPriceAmountWithoutDiscount + ((cartTotalPriceAmountWithoutDiscount - couponDiscount - giftCardDiscount) * 12) / 100) + parseInt(shipping))}</td>
+                                <td className='flex justify-end font-bold'>$ {cartTotalPriceAmountWithoutDiscount && ((cartTotalPriceAmountWithoutDiscount + ((cartTotalPriceAmountWithoutDiscount - couponDiscount - giftCardDiscount) * 12) / 100) + parseInt(shipping)).toFixed(2)}</td>
                             </tr>
                         </tbody>
                     </table>
